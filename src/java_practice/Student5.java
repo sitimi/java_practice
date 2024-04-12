876996ed34c95bc32419e1faa86675dddebe6e3a
ryoji <sitimi522@gmail.com>
2024-04-12 13:43:02 +0900

    カプセル化練習

---
+++ src/java_practice/Student5.java
@@ -0,0 +1,21 @@
+package java_practice;
+
+public class Student5 {
+	private String name;
+	private int score;
+	
+	public Student5(String n) {
+		name = n;
+	}
+	public void setScore(int s) {
+		if (0 <= s && s <= 100) {
+			score = s;
+		}else {
+			System.out.println(name + "さんの点数が不正です");
+			score = 0;
+		}
+	}
+	void display(){
+		System.out.println(name + "さん" + score + "点");
+	}
+}
--- src/java_practice/Stusample4.java
+++ src/java_practice/Stusample4.java
@@ -3,11 +3,13 @@ package java_practice;
 public class Stusample4 {
 
 	public static void main(String[] args) {
-		Student4 stu1 = new Student4("大成");
-		Student4.display();
+		Student5 stu1 = new Student5("大成");
+		stu1.setScore(80);
+		stu1.display();
 		
-		Student4 stu2 = new Student4("田中");
-		Student4.display();
+		Student5 stu2 = new Student5("田中");
+		stu2.setScore(-50);
+		stu2.display();
 
 	}
 
