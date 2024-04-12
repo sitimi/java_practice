5372fd149b47825845e150ebf78e21a634ba682b
ryoji <sitimi522@gmail.com>
2024-04-12 20:00:12 +0900

    this super練習

---
+++ src/java_practice/Person3.java
@@ -0,0 +1,13 @@
+package java_practice;
+
+public class Person3 {
+	private String name;
+	
+	public Person3(String name) {
+		this.name = name;
+	}
+	public void display() {
+		System.out.println("名前：" + name);
+	}
+
+}
--- src/java_practice/StuSample.java
+++ src/java_practice/StuSample.java
@@ -3,10 +3,9 @@ package java_practice;
 public class StuSample {
 
 	public static void main(String[] args) {
-		Student2 stu = new Student2();
+		Student3 stu = new Student3("大成");
 		
-		stu.setName("大成");	
-		stu.setStuNo(1);
+
 		stu.display();
 
 	}
