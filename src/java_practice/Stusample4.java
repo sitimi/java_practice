e95afe983a3837ee5e6dfda343a99945aca2ffaa
ryoji <sitimi522@gmail.com>
2024-04-12 13:31:17 +0900

    static練習

---
+++ src/java_practice/Stusample4.java
@@ -0,0 +1,14 @@
+package java_practice;
+
+public class Stusample4 {
+
+	public static void main(String[] args) {
+		Student4 stu1 = new Student4("大成");
+		Student4.display();
+		
+		Student4 stu2 = new Student4("田中");
+		Student4.display();
+
+	}
+
+}
