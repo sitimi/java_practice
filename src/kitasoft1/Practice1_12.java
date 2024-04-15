8bd56b1595c02f03ff13fcb27988c0ea15d227cc
ryoji <sitimi522@gmail.com>
2024-04-16 00:36:32 +0900

    練習問題 1 - 12

---
+++ src/kitasoft1/Practice1_12.java
@@ -0,0 +1,12 @@
+package kitasoft1;
+
+public class Practice1_12 {
+
+	public static void main(String[] args) {
+		int x = 10;
+		
+		System.out.println( x++ );
+		System.out.println( x-- );
+	}
+
+}
