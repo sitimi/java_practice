785ba998bce9d241b1a02410f54d1cafd85641bc
ryoji <sitimi522@gmail.com>
2024-04-16 00:08:35 +0900

    練習問題 1 - 3

---
+++ src/kitasoft1/DisplayTwoNum.java
@@ -0,0 +1,12 @@
+package kitasoft1;
+
+public class DisplayTwoNum {
+
+	public static void main(String[] args) {
+		int x = 13;
+		int y = 17;
+		System.out.println("x=" + x + "," + "y=" + y);
+
+	}
+
+}
