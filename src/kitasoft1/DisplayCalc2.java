295c2e8151a8404298c118446fdbe8bb7eb171d3
ryoji <sitimi522@gmail.com>
2024-04-16 00:15:15 +0900

    練習問題 1 - 6

---
+++ src/kitasoft1/DisplayCalc2.java
@@ -0,0 +1,14 @@
+package kitasoft1;
+
+public class DisplayCalc2 {
+
+	public static void main(String[] args) {
+		int x = 7;
+		x *= 3;
+		System.out.println(x);
+		x /= 2;
+		System.out.println(x);
+
+	}
+
+}
