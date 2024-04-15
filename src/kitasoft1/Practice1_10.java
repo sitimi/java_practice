eeae465f6d9c40cccdc19bf10e4f89b578e09095
ryoji <sitimi522@gmail.com>
2024-04-16 00:30:00 +0900

    練習問題 1 - 10

---
+++ src/kitasoft1/Practice1_10.java
@@ -0,0 +1,18 @@
+package kitasoft1;
+
+public class Practice1_10 {
+
+	public static void main(String[] args) {
+		int x = 2;
+		int y;
+		
+		y = x;
+		System.out.println(y);
+		y = x * x;
+		System.out.println(y);
+		y = x * x * x;
+		System.out.println(y);
+
+	}
+
+}
