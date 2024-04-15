4e0ee61f5bb90a12b6154793daa22a768b7accab
ryoji <sitimi522@gmail.com>
2024-04-16 00:20:38 +0900

    練習問題 1 - 8

---
+++ src/kitasoft1/Practice1_8.java
@@ -0,0 +1,17 @@
+package kitasoft1;
+
+public class Practice1_8 {
+
+	public static void main(String[] args) {
+		int x = 19;
+		int y = 23;
+		int z;
+		
+		z = x * y;
+		
+		System.out.println(z);
+
+
+	}
+
+}
