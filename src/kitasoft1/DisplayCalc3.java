f997e9bdd2517833aee6dbe29a0059853009df7c
ryoji <sitimi522@gmail.com>
2024-04-16 00:17:27 +0900

    練習問題 1 - 7

---
+++ src/kitasoft1/DisplayCalc3.java
@@ -0,0 +1,16 @@
+package kitasoft1;
+
+public class DisplayCalc3 {
+
+	public static void main(String[] args) {
+		int x = 3;
+		int y = 7;
+		int z;
+		z = x;
+		x = y;
+		y = z;
+		System.out.println( "x=" + x + ",y=" + y );
+
+	}
+
+}
