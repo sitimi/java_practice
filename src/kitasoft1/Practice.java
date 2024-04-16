43c21835c01c51aa140aaafe16c9fe0983087be2
ryoji <sitimi522@gmail.com>
2024-04-16 23:09:37 +0900

    practice

--- src/kitasoft1/Practice.java
+++ src/kitasoft1/Practice.java
@@ -6,20 +6,21 @@ public class Practice {
 
 	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int x = Integer.parseInt(br.readLine());
-		int y = Integer.parseInt(br.readLine());
+		int[][] schedule = {{0,1,0,1,1,0,1},{0,1,1,1,1,1,0},{0,1,1,0,1,1,0}};
 		
-		boolean judge;
-		judge = (x >= 60 && y >= 60) || (x + y >= 130) || 
-				(x + y >= 100 && (x  >= 90 || y >= 90));
+		System.out.println("曜日を入力してください\n"
+				+ "0=日曜、1=月曜、2=火曜、3=水曜、4=木曜、5=金曜、6=土曜");
+		int day = Integer.parseInt(br.readLine());
 		
-		if(judge){
-			System.out.println("合格");
+		System.out.println("時間帯を入力してください\n"
+				+ "0=午前、1=午後、2=夜間");
+		int time = Integer.parseInt(br.readLine());
+		
+		if(schedule[time][day] == 0) {
+			System.out.println("休診");
 		}else {
-			System.out.println("不合格");
+			System.out.println("開いています");
 		}
-		
-		
-		}
+	}
 	
 }
