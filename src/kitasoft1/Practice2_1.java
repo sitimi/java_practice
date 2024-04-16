2efd55284445dacbec6c1e96aee8627aba8d87f0
ryoji <sitimi522@gmail.com>
2024-04-16 18:30:03 +0900

    練習問題 2 - 1

---
+++ src/kitasoft1/Practice2_1.java
@@ -0,0 +1,16 @@
+package kitasoft1;
+
+import java.io.*;
+
+public class Practice2_1 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(
+                new InputStreamReader( System.in ));
+		
+		int s = Integer.parseInt( br.readLine() );
+		
+		System.out.println(s);
+	}
+
+}
