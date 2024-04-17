99f96ed677c0d171ff068ff36655b3641be1cc57
ryoji <sitimi522@gmail.com>
2024-04-17 22:01:25 +0900

    練習問題 4 - 7

--- src/kitasoft1/Practice.java
+++ src/kitasoft1/Practice.java
@@ -11,15 +11,14 @@ public class Practice {
 		int score2 = 0;
 
 		for (int i = 1; i <= 9; i++) {
-		
-				System.err.println(i + "回表、巨人の得点は？");
-				score1 += Integer.parseInt(br.readLine());
-		
-				System.err.println(i + "回裏、阪神の得点は？");
-				score2 += Integer.parseInt(br.readLine());
-			}
 
-		
+			System.err.println(i + "回表、巨人の得点は？");
+			score1 += Integer.parseInt(br.readLine());
+
+			System.err.println(i + "回裏、阪神の得点は？");
+			score2 += Integer.parseInt(br.readLine());
+		}
+
 		System.out.println("巨人：" + score1 + "点、阪神：" + score2 + "点");
 		if (score1 == score2) {
 			System.out.println("引き分け");
