5f50264018deeae91ecc24aad7af8ec82541200b
ryoji <sitimi522@gmail.com>
2024-04-19 15:07:08 +0900

    練習問題 4 - 8

--- src/kitasoft1/Practice.java
+++ src/kitasoft1/Practice.java
@@ -6,27 +6,16 @@ public class Practice {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = 0;
 
-		int score1 = 0;
-		int score2 = 0;
-
-		for (int i = 1; i <= 9; i++) {
-
-			System.err.println(i + "回表、巨人の得点は？");
-			score1 += Integer.parseInt(br.readLine());
-
-			System.err.println(i + "回裏、阪神の得点は？");
-			score2 += Integer.parseInt(br.readLine());
+		System.err.println("自然数を１０回入力してください");
+		for (int i = 1; i <= 10; i++) {
+			int x = Integer.parseInt(br.readLine());
+			if (num <= x) {
+				num = x;
+			}
 		}
-
-		System.out.println("巨人：" + score1 + "点、阪神：" + score2 + "点");
-		if (score1 == score2) {
-			System.out.println("引き分け");
-		} else if (score1 > score2) {
-			System.out.println("巨人の勝ち");
-		} else {
-			System.out.println("阪神の勝ち");
-		}
+		System.out.println(num);
 
 	}
 
