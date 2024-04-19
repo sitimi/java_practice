fb3b7e6853d155879e8f1010fad11f99b33a6ff6
ryoji <sitimi522@gmail.com>
2024-04-19 22:19:53 +0900

    練習問題 4 - 16

--- src/kitasoft1/Practice.java
+++ src/kitasoft1/Practice.java
@@ -6,22 +6,18 @@ public class Practice {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int i;
-		
-		int num = Integer.parseInt(br.readLine());
-		
-		for (i = 2; i <= (num / 2); i++) {
-			if(num % i == 0) {
-				break;
-			}
-			
-		}
-		if(i <= (num / 2)) {
-			System.out.println(num + "は素数ではありません");
-		}else {
-			System.out.println(num + "は素数です");
-		}
+		int i = 2;
 
+		int num = Integer.parseInt(br.readLine());
+
+		while (num > 1) {
+			while (num % i == 0) {
+				System.out.print(i + " ");
+				num /= i;
+
+			}
+			i++;
+		}
 	}
 
 }
