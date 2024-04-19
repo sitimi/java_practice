6657e7eb1b51e90ca02588fc1fcd793864a71bec
ryoji <sitimi522@gmail.com>
2024-04-19 15:53:25 +0900

    練習問題 4 - 14

--- src/kitasoft1/Practice.java
+++ src/kitasoft1/Practice.java
@@ -8,17 +8,18 @@ public class Practice {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int strike = 0;
 		int ball = 0;
+		int foul = 0;
 
-		
 		while (strike < 3 && ball < 4) {
+			System.out.println("ストライク=1 or ボール=2 or ファール=3？");
 			int judge = Integer.parseInt(br.readLine());
-			
-			if(judge == 1) {
-				strike++;
-			}else if(judge == 2){
+
+			if (judge == 2) {
 				ball++;
+			} else if ((judge == 3 && strike < 2)|| judge == 1) {
+				strike++;
 			}
-			
+
 		}
 		System.out.println(ball + "ボール、" + strike + "ストライク");
 
