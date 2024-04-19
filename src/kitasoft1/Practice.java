fe2578d1c397ae2519cc7ded68ff10a8b2e41468
ryoji <sitimi522@gmail.com>
2024-04-19 22:04:45 +0900

    練習問題 4 - 15

--- src/kitasoft1/Practice.java
+++ src/kitasoft1/Practice.java
@@ -6,22 +6,21 @@ public class Practice {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int strike = 0;
-		int ball = 0;
-		int foul = 0;
-
-		while (strike < 3 && ball < 4) {
-			System.out.println("ストライク=1 or ボール=2 or ファール=3？");
-			int judge = Integer.parseInt(br.readLine());
-
-			if (judge == 2) {
-				ball++;
-			} else if ((judge == 3 && strike < 2)|| judge == 1) {
-				strike++;
+		int i;
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		for (i = 2; i <= (num / 2); i++) {
+			if(num % i == 0) {
+				break;
 			}
-
+			
 		}
-		System.out.println(ball + "ボール、" + strike + "ストライク");
+		if(i <= (num / 2)) {
+			System.out.println(num + "は素数ではありません");
+		}else {
+			System.out.println(num + "は素数です");
+		}
 
 	}
 
