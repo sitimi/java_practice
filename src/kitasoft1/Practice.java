7bc41da5db08245be1b44eac84a594381462be71
ryoji <sitimi522@gmail.com>
2024-04-19 15:14:45 +0900

    練習問題 4 - 10

--- src/kitasoft1/Practice.java
+++ src/kitasoft1/Practice.java
@@ -6,21 +6,15 @@ public class Practice {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int max_num = 0;
-		int min_num = 0;
-
-		System.out.println("整数を１０回入力してください");
-		for (int i = 1; i <= 10; i++) {
-			int x = Integer.parseInt(br.readLine());
-			if (max_num < x) {
-				max_num = x;
-			}else if(min_num > x){
-				min_num = x;
-			}
-			
+		int count = 0;
+		
+		count = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i <= count; i++) {
+			System.out.print("*");
 		}
-		System.out.println(max_num);
-		System.out.println(min_num);
+		
+
 
 
 	}
