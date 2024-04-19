30be41ccac36e778d7f8e91b2281813c8cff6048
ryoji <sitimi522@gmail.com>
2024-04-19 15:21:53 +0900

    練習問題 4 - 11

--- src/kitasoft1/Practice.java
+++ src/kitasoft1/Practice.java
@@ -6,16 +6,13 @@ public class Practice {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int count = 0;
-		
-		count = Integer.parseInt(br.readLine());
-		
-		for(int i = 1; i <= count; i++) {
-			System.out.print("*");
+		int count = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < count; i++) {
+
+			System.out.print(i % 10);
+
 		}
-		
-
-
 
 	}
 
